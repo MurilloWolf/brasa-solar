53fb063bbe731781d5da78aec52b687879ccd9ec
MurilloWolf <mwolfcavalheiro@gmail.com>
2024-11-29 17:02:33 -0300

    feat: added number on contact-button

--- src/components/system/ContactButton/ContactButton.tsx
+++ src/components/system/ContactButton/ContactButton.tsx
@@ -4,7 +4,9 @@ import { useState, useEffect } from "react";
 
 export default function ContactButton() {
   const [isVisible, setIsVisible] = useState(false);
-
+  const whatsappMessage =
+    "Olá, gostaria de solicitar um orçamento para instalação de placas solares";
+  const whatsappNumber = "5518996556202";
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 900) {
@@ -23,9 +25,21 @@ export default function ContactButton() {
 
   if (!isVisible) return null;
 
+  const handleRedirectWhatsApp = () => {
+    window.open(
+      `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+        whatsappMessage
+      )}`,
+      "_blank"
+    );
+  };
+
   return (
     <div className="md:hidden fixed bottom-0 left-0 w-full bg-black/80 flex justify-center items-center p-4 py-8 pb-12">
-      <Button className="bg-orange-500 hover:bg-orange-400 w-full text-lg ">
+      <Button
+        className="bg-orange-500 hover:bg-orange-400 w-full text-lg "
+        onClick={handleRedirectWhatsApp}
+      >
         Solicitar orçamento <CircleDollarSign />
       </Button>
     </div>
