e0178e1c7a80f71e534155d1656200b2434d1cb3
MurilloWolf <mwolfcavalheiro@gmail.com>
2024-12-21 10:12:45 -0300

    feat: increment seo

---
+++ src/components/system/Metatags/Metatags.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+export default function Metatags() {
+  return (
+    <Head>
+      <meta
+        name="description"
+        content="Brasa Solar - Especialistas em energia solar! Reduza sua conta de luz e contribua para um futuro sustentável. Peça um orçamento em Presidente Prudente e região hoje mesmo!"
+      />
+      <meta
+        name="keywords"
+        content="energia solar em Presidente Prudente, placas solares, orçamento energia solar, calculadora de economia solar, soluções sustentáveis oeste paulista"
+      />
+      <meta name="author" content="Brasa Solar" />
+      <meta
+        property="og:title"
+        content="Brasa Solar - Soluções de Energia Solar em Presidente Prudente"
+      />
+      <meta
+        property="og:description"
+        content="Reduza sua conta de luz com a Brasa Solar. Oferecemos placas solares de alta qualidade em Presidente Prudente e região. Faça um orçamento e economize agora!"
+      />
+      <meta
+        property="og:image"
+        content="https://www.brasasolar.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-simplify.c4c1aa3e.png&w=96&q=75"
+      />
+      <meta property="og:url" content="https://www.brasasolar.com" />
+      <link rel="canonical" href="https://www.brasasolar.com" />
+    </Head>
+  );
+}
